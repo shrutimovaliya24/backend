6c47f6ca41f74371d05ecccc587078318071846a
quizyantra <quizyantra@gmail.com>
2025-07-14 18:16:18 +0530

    new  commit

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
-
 const webhookRoute = require("./routes/webhook");
 
 const app = express();
 
-app.use(cors());
-app.use(express.text()); // Needed to receive plain text token
+app.use(cors({
+  origin: "https://fronted-git-main-shrutimovaliya24-gmailcoms-projects.vercel.app"
+}));
+
+app.use(express.text());
 
 app.use("/", webhookRoute);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(3000, () => console.log("Server running"));
