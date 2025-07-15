54890cf99f727fbbb289e1c7ab26c282192c3def
vasu <vasudesai1207@gmail.com>
2025-07-15 12:45:00 +0530

    few changes

--- server.js
+++ server.js
@@ -29,5 +29,5 @@ app.use((err, req, res, next) => {
   res.status(500).send("Internal server error");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
