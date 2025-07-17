ac774c3b63f78fa38a7144adb25c1e5c8f878263
quizyantra <quizyantra@gmail.com>
2025-07-17 19:13:57 +0530

    database edit

--- config/db.js
+++ config/db.js
@@ -1,19 +1,12 @@
-const mysql = require('mysql2');
+const mysql = require('mysql');
 require('dotenv').config();
 
-const connection = mysql.createConnection({
-    host: process.env.MYSQLHOST || 'localhost',
-    user: process.env.MYSQLUSER || 'root',
-    password: process.env.MYSQLPASSWORD || '',
-    database: process.env.MYSQLDATABASE || 'googlereview'
+const pool = mysql.createPool({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+ 
 });
 
-connection.connect((err) => {
-  if (err) {
-    console.error('MySQL connection error:', err);
-    return;
-  }
-  console.log('Connected to MySQL database');
-});
-
-module.exports = connection;
+module.exports = pool;
